9133a9e5232cfeb759fe392109f7f6a1f33de0c4
jeremie <pro_rish@yahoo.com>
2016-06-15 20:36:45 +0200

    ajout logo_lk and some jquery stuff

---
+++ makit/js/script.js
@@ -0,0 +1,21 @@
+$(function() {
+
+$('.logo_claire').hide();
+
+		$('.socialhover_claire').hover(
+			function () {
+				$('.logo_claire').fadeIn();
+			}, function () {
+				$('.logo_claire').fadeOut();
+		});
+
+$('.logo_franck').hide();
+
+		$('.socialhover_franck').hover(
+			function () {
+				$('.logo_franck').fadeIn();
+			}, function () {
+				$('.logo_franck').fadeOut();
+		});
+
+});// document ready
